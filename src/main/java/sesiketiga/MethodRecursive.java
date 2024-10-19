9fb96ab7a7b292c819a2a68e74eff5642d476731
Azka Hanifah <azkahanifah@Azkas-MacBook-Pro.local>
2024-10-19 11:08:35 +0700

    feat: sesi ketiga

---
+++ src/main/java/sesiketiga/MethodRecursive.java
@@ -0,0 +1,23 @@
+package sesiketiga;
+
+public class MethodRecursive {
+
+    public static void main(String[] args) {
+        /**
+         * METHOD RECURSIVE
+         * Sebuah teknik memanggil dirinya sendiri
+         */
+
+        int input = 5;
+        int factorial = calculateFactorial(input);
+        System.out.println("Faktorial dari " + input + " adalah: " + factorial);
+    }
+
+    public static int calculateFactorial(int input) {
+        if (input == 0) {
+            return 1;
+        } else {
+            return input * calculateFactorial(input - 1);
+        }
+    }
+}
